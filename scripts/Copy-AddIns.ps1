d1377477b7ff008c74f8476492b0b8cd40cb0695
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-01-07 13:12:13 +0000

    Allow zipped add-ins

--- scripts/Copy-AddIns.ps1
+++ scripts/Copy-AddIns.ps1
@@ -7,12 +7,17 @@ $settings = (Get-Content ((Get-ChildItem -Path 'C:\Build' -Filter "build-setting
 Write-Host "Copying Add-ins to the service tier add-ins folder"
 
 $settings.dotnetAddIns | ForEach-Object {
-    $appFile = $_
-    if ($appFile.ToLower().StartsWith("http://") -or $appFile.ToLower().StartsWith("https://")) {
-        $appUrl = $appFile
-        $name = [System.Uri]::UnescapeDataString([System.IO.Path]::GetFileName($appUrl).split("?")[0])
-        $appFile = Join-Path $serviceTierAddInsFolder $name
-        Download-File -sourceUrl $appUrl -destinationFile $appFile
+    $addinFile = $_
+    if ($addinFile.ToLower().StartsWith("http://") -or $addinFile.ToLower().StartsWith("https://")) {
+        $addinUrl = $addinFile
+        $name = [System.Uri]::UnescapeDataString([System.IO.Path]::GetFileName($addinUrl).split("?")[0])
+        $addinFile = Join-Path $serviceTierAddInsFolder $name
+        Download-File -sourceUrl $addinUrl -destinationFile $addinFile
+        if ($addinFile.EndsWith(".zip", "OrdinalIgnoreCase")) {
+            Write-Host "Extracting .zip file " -NoNewline
+            Expand-7zipArchive -Path $addinFile -DestinationPath $serviceTierAddInsFolder
+            Remove-Item -Path $addinFile -Force
+        }        
     }
 }
 
