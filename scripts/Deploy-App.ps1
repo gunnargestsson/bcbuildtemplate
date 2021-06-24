e5f64115e1345df70c2cc7b086a2d60cf8d4a1fd
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-06-24 12:14:26 +0000

    Deploy using S2S authentication

--- scripts/Deploy-App.ps1
+++ scripts/Deploy-App.ps1
@@ -15,7 +15,7 @@
     $clientId = $ENV:CLIENTID,
     
     [Parameter(Mandatory = $false)]
-    $refreshToken = $ENV:REFRESHTOKEN
+    $clientSecret = $ENV:CLIENTSECRET
     
 )
 
@@ -105,8 +105,10 @@ foreach ($deployment in $deployments) {
         elseif ($deploymentType -eq "onlineTenant") {
             $environment = $deployment.DeployToName;
             $tenantId = $deployment.DeployToTenants | Select-Object -First 1
+            if ($clientSecret -is [String]) { $clientSecret = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force }
+            if ($clientSecret -isnot [SecureString]) { throw "ClientSecret needs to be a SecureString or a String" }
             Write-Host "Online Tenant deployment to https://businesscentral.dynamics.com/${tenantId}/${environment}/"
-            $authContext = New-BcAuthContext -clientID $clientId -refreshToken $refreshToken -tenantID $tenantId -scopes "https://api.businesscentral.dynamics.com/.default" -includeDeviceLogin
+            $authContext = New-BcAuthContext -clientID $clientId -clientSecret $clientSecret -tenantID $tenantId -scopes "https://api.businesscentral.dynamics.com/.default"
             Publish-PerTenantExtensionApps -bcAuthContext $authContext -environment $environment -appFiles $appFile -Verbose
         }
         elseif ($deploymentType -eq "container" -and ($deployment.DeployToTenants).Count -eq 0) {
