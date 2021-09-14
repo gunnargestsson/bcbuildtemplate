d09d490bb6ea0493269209812d3af5a99bfc00b9
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-09-14 14:55:19 +0000

    Copy Script files

--- scripts/Copy-Template.ps1
+++ scripts/Copy-Template.ps1
@@ -3,4 +3,4 @@ $tempFolder = Join-Path $env:TEMP (New-Guid)
 Write-Host "Set templateFolder = $tempFolder"
 Write-Host "##vso[task.setvariable variable=templateFolder]$tempFolder"
 
-Copy-Item -Path .\bcbuildtemplate\scripts -Destination $tempFolder
+Copy-Item -Path $PSScriptRoot -Destination $tempFolder -Recurse
