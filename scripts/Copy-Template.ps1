27035cea4b977dda3e3621d304d61adc9320e4a0
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-09-14 15:32:26 +0000

    Folder path changes

--- scripts/Copy-Template.ps1
+++ scripts/Copy-Template.ps1
@@ -1,6 +1,6 @@
 
-$tempFolder = Join-Path $env:TEMP (New-Guid)
+$tempFolder = Join-Path (Join-Path $ENV:AGENT_TOOLSDIRECTORY 'bcbuildtemplate')
 Write-Host "Set templateFolder = $tempFolder"
 Write-Host "##vso[task.setvariable variable=templateFolder]$tempFolder"
 
-Copy-Item -Path $PSScriptRoot -Destination $tempFolder -Recurse
+Copy-Item -Path $PSScriptRoot -Destination $tempFolder -Recurse -Force
