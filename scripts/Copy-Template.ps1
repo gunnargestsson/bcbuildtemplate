a84bc4e2bf61eba2463df0bb08c49a45fecc4086
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-09-14 16:41:10 +0000

    Update copy template

--- scripts/Copy-Template.ps1
+++ scripts/Copy-Template.ps1
@@ -7,4 +7,4 @@ $tempFolder = Join-Path $AgentToolsDirectory 'bcbuildtemplate'
 Write-Host "Set templateFolder = $tempFolder"
 Write-Host "##vso[task.setvariable variable=templateFolder]$tempFolder"
 
-Copy-Item -Path $PSScriptRoot -Destination $tempFolder -Recurse -Force
+Copy-Item -Path (Join-Path $PSScriptRoot "*.ps1") -Destination $tempFolder -Recurse -Force
