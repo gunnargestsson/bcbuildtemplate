b1bdd0897d127a6baa3b3d0fc245dd352e44a520
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-09-17 15:40:49 +0000

    change to release folder

--- scripts/CopyTo-LatestFolder.ps1
+++ scripts/CopyTo-LatestFolder.ps1
@@ -6,18 +6,21 @@ Param(
     [Parameter(Mandatory = $false)]
     [string] $buildArtifactFolder = $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY,
 
+    [Parameter(Mandatory = $false)]
+    [string] $folder = 'current',
+
     [Parameter(Mandatory = $true)]
     [string] $appFolders
 )
 
 
-$LatestPath = Join-Path $buildArtifactFolder 'latest';
-New-Item -Path $LatestPath -ItemType Directory;
+$DestinationPath = Join-Path $buildArtifactFolder $folder;
+New-Item -Path $DestinationPath -ItemType Directory;
 foreach ($folder in ($appFolders.Split(','))) {
     $App = @(Get-ChildItem (Join-Path $buildArtifactFolder $folder) -Filter '*.app') | Select-Object -First 1
     $AppJson = @(Get-ChildItem (Join-Path $buildArtifactFolder $folder) -Filter 'app.json') | Select-Object -First 1
     $AppConfig = Get-Content -Path $AppJson | ConvertFrom-Json
-    New-Item -Path (Join-Path $LatestPath $AppConfig.publisher) -ItemType Directory -ErrorAction SilentlyContinue
-    $AppFileName = "$(Join-Path (Join-Path $LatestPath $AppConfig.publisher) $AppConfig.Name).app"            
+    New-Item -Path (Join-Path $DestinationPath $AppConfig.publisher) -ItemType Directory -ErrorAction SilentlyContinue
+    $AppFileName = "$(Join-Path (Join-Path $DestinationPath $AppConfig.publisher) $AppConfig.Name).app"            
     Copy-Item -Path $App -Destination $AppFileName -Verbose
 }
