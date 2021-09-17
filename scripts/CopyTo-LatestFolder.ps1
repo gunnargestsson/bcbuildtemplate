0050bbd8031d98b0a6df8dcb06514b8722c092b0
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-09-17 16:04:01 +0000

    update copy

--- scripts/CopyTo-LatestFolder.ps1
+++ scripts/CopyTo-LatestFolder.ps1
@@ -16,10 +16,11 @@ Param(
 
 $DestinationPath = Join-Path $buildArtifactFolder $releaseFolder;
 New-Item -Path $DestinationPath -ItemType Directory;
-foreach ($folder in ($appFolders.Split(','))) {
-    $AppFolder = Join-Path $buildArtifactFolder $folder
-    $App = @(Get-ChildItem $AppFolder -Filter '*.app') | Select-Object -First 1
-    $AppJson = @(Get-ChildItem $AppFolder -Filter 'app.json') | Select-Object -First 1
+$appFolders.Split(',') | ForEach-Object {
+    $AppFolder = Join-Path $buildArtifactFolder $_
+    Write-Host "Reading App information in ${AppFolder}"
+    $App = (Get-Item -Path (Join-Path $AppFolder '*.app')).FullName
+    $AppJson = (Get-Item -Path (Join-Path $AppFolder 'app.json')).FullName
     $AppConfig = Get-Content -Path $AppJson | ConvertFrom-Json
     New-Item -Path (Join-Path $DestinationPath $AppConfig.publisher) -ItemType Directory -ErrorAction SilentlyContinue
     $AppFileName = "$(Join-Path (Join-Path $DestinationPath $AppConfig.publisher) $AppConfig.Name).app"            
