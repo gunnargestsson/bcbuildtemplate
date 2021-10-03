a96ed2a617e1d20f33ab427ea065de33a4b20719
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-10-03 13:40:57 +0000

    Remove invalid characters from file names

--- scripts/CopyTo-LatestFolder.ps1
+++ scripts/CopyTo-LatestFolder.ps1
@@ -13,6 +13,19 @@ Param(
     [string] $appFolders
 )
 
+Function Remove-InvalidFileNameChars {
+    param(
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [String]$Name
+    )
+  
+    $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
+    $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
+    return ($Name -replace $re)
+}
 
 $DestinationPath = Join-Path $buildArtifactFolder $releaseFolder;
 New-Item -Path $DestinationPath -ItemType Directory;
@@ -23,6 +36,6 @@ $appFolders.Split(',') | ForEach-Object {
     $AppJson = (Get-Item -Path (Join-Path $AppFolder 'app.json')).FullName
     $AppConfig = Get-Content -Path $AppJson | ConvertFrom-Json
     New-Item -Path (Join-Path $DestinationPath $AppConfig.publisher) -ItemType Directory -ErrorAction SilentlyContinue
-    $AppFileName = "$(Join-Path (Join-Path $DestinationPath $AppConfig.publisher) $AppConfig.Name).app"            
+    $AppFileName = "$(Join-Path (Join-Path $DestinationPath Remove-InvalidFileNameChars($AppConfig.publisher)) Remove-InvalidFileNameChars($AppConfig.Name)).app"
     Copy-Item -Path $App -Destination $AppFileName -Verbose
 }
