4ae36f0d8953af373713cb49ac62a8245bd78219
Gunnar Thor Gestsson <gunnar@navision.guru>
2021-10-29 08:17:02 +0000

    Update New-LocalDevEnv.ps1

--- tempate/New-LocalDevEnv.ps1
+++ tempate/New-LocalDevEnv.ps1
@@ -86,4 +86,66 @@ else {
     if ($settings.testToolkitCountry) {
         Import-TestToolkitToBcContainer -containerName $containerName -testToolkitCountry $settings.testToolkitCountry
     }
+
+    # Update Launch Json for all apps
+    $apps = Get-ChildItem -Path (Split-Path -Path $scriptPath -Parent) -Filter "app.json" -Recurse
+    foreach ($appFolder in $apps.DirectoryName) {
+        $vscodeFolder = Join-Path $appFolder ".vscode"
+        New-Item -Path $vscodeFolder -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+        $launchJsonFile = Join-Path $vscodeFolder "launch.json"
+        if (Test-Path $launchJsonFile) {
+            Write-Host "Modifying $launchJsonFile"
+            $launchJson = Get-Content $LaunchJsonFile | ConvertFrom-Json
+        }
+        else {
+            Write-Host "Creating $launchJsonFile"
+            $dir = [System.IO.Path]::GetDirectoryName($launchJsonFile)
+            if (!(Test-Path $dir)) {
+                New-Item -Path $dir -ItemType Directory | Out-Null
+            }
+            $launchJson = @{ "version" = "0.2.0"; "configurations" = @() } | ConvertTo-Json | ConvertFrom-Json
+        }
+
+        $config = Get-BcContainerServerConfiguration -ContainerName $containerName
+        if ($config.DeveloperServicesSSLEnabled -eq "true") {
+            $devserverUrl = "https://$containerName"
+        }
+        else {
+            $devserverUrl = "http://$containerName"
+        }
+        if ($config.ClientServicesCredentialType -eq "Windows") {
+            $authentication = "Windows"
+        }
+        else {
+            $authentication = "UserPassword"
+        }
+
+        $launchSettings = [ordered]@{
+            "type"           = 'al'
+            "request"        = 'launch'
+            "name"           = $containerName
+            "server"         = $devserverUrl
+            "serverInstance" = $config.ServerInstance
+            "port"           = [int]($config.DeveloperServicesPort)
+            "tenant"         = 'default'
+            "authentication" = $authentication
+            "breakOnError"   = $true
+            "launchBrowser"  = $true
+        }      
+        
+        $launchSettings | ConvertTo-Json | Out-Host
+        $oldSettings = $launchJson.configurations | Where-Object { $_.name -eq $launchsettings.name }
+        if ($oldSettings) {
+            $oldSettings.PSObject.Properties | % {
+                $prop = $_.Name
+                if (!($launchSettings.Keys | Where-Object { $_ -eq $prop } )) {
+                    $launchSettings += @{ "$prop" = $oldSettings."$prop" }
+                }
+            }
+        }
+        $launchJson.configurations = @($launchJson.configurations | Where-Object { $_.name -ne $launchsettings.name })
+        $launchJson.configurations += $launchSettings
+        $launchJson | ConvertTo-Json -Depth 10 | Set-Content $launchJsonFile
+
+    }
 }
