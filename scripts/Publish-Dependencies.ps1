497a8f474c9959f8823e4d28f0e40d72fbe0f92f
Gunnar Gestsson <gunnar@navision.guru>
2023-05-02 14:11:19 +0000

    fix zip logic

--- scripts/Publish-Dependencies.ps1
+++ scripts/Publish-Dependencies.ps1
@@ -18,21 +18,23 @@ Param(
 $settings = (Get-Content -Path $configurationFilePath -Encoding UTF8 | Out-String | ConvertFrom-Json)
 $settings.dependencies | ForEach-Object {
     Write-Host "Publishing $_"
-    
+        
     $guid = New-Guid
-    $appFile = Join-Path $env:TEMP $guid.Guid
-    Write-Host "Downloading app file $($_) to $($appFile)"    
-    Download-File -sourceUrl $_ -destinationFile $appFile
-    if ($_.EndsWith(".zip", "OrdinalIgnoreCase") -or $_.Contains(".zip?")) {
-        $guid = New-Guid
+    if ($_.EndsWith(".zip", "OrdinalIgnoreCase") -or $_.Contains(".zip?")) {        
         $appFolder = Join-Path $env:TEMP $guid.Guid
         $appFile = Join-Path $env:TEMP "$($guid.Guid).zip"
+        Write-Host "Downloading app file $($_) to $($appFile)"        
+        Download-File -sourceUrl $_ -destinationFile $appFile
         New-Item -ItemType Directory -Path $appFolder -Force | Out-Null
         Write-Host "Extracting .zip file "
         Expand-Archive -Path $appFile -DestinationPath $appFolder
         Remove-Item -Path $appFile -Force
         $appFile = Get-ChildItem -Path $appFolder -Recurse -Include *.app -File | Select-Object -First 1
-    }  
+    }  else {
+        Write-Host "Downloading app file $($_) to $($appFile)"        
+        $appFile = Join-Path $env:TEMP $guid.Guid    
+        Download-File -sourceUrl $_ -destinationFile $appFile
+    }
 
     Write-Host "Container deployment to ${containerName}"
     Publish-BCContainerApp -containerName $containerName -appFile $appFile -skipVerification -scope Global
