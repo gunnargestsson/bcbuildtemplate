c813cb76ae960115a3c34fd90e202d1e7008dc8a
Gunnar Thor Gestsson <gunnar@navision.guru>
2023-09-13 12:51:10 +0000

    Remove allow prelease

--- scripts/Install-BCContainerHelper.ps1
+++ scripts/Install-BCContainerHelper.ps1
@@ -11,13 +11,13 @@ if ($module) {
     Write-Host "bccontainerhelper $VersionStr is installed"
     if ($bccontainerhelperVersion -eq "latest") {
         Write-Host "Determine latest bccontainerhelper version"
-        $latestVersion = (Find-Module -Name bccontainerhelper -AllowPrerelease).Version
+        $latestVersion = (Find-Module -Name bccontainerhelper).Version
         $bccontainerhelperVersion = $latestVersion.ToString()
         Write-Host "bccontainerhelper $bccontainerhelperVersion is the latest version"
     }
     if ($bccontainerhelperVersion -ne $module.Version) {
         Write-Host "Updating bccontainerhelper to $bccontainerhelperVersion"
-        Update-Module -Name bccontainerhelper -Force -RequiredVersion $bccontainerhelperVersion -AllowPrerelease
+        Update-Module -Name bccontainerhelper -Force -RequiredVersion $bccontainerhelperVersion
         Write-Host "bccontainerhelper updated"
     }
 }
@@ -28,7 +28,7 @@ else {
     }
     if ($bccontainerhelperVersion -eq "latest") {
         Write-Host "Installing bccontainerhelper"
-        Install-Module -Name bccontainerhelper -AllowPrerelease -Force
+        Install-Module -Name bccontainerhelper -Force
     }
     else {
         Write-Host "Installing bccontainerhelper version $bccontainerhelperVersion"
