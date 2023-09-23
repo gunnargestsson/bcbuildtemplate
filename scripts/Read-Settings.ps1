6da4637ab7315fd9bd0b0f77476310c7d5428e4a
Gunnar Thor Gestsson <gunnar@navision.guru>
2023-09-23 13:42:02 +0000

    Add quotes

--- scripts/Read-Settings.ps1
+++ scripts/Read-Settings.ps1
@@ -56,7 +56,7 @@ if ($changesOnly) {
     if ([String]::IsNullOrEmpty($target)) {
         $files=$(git diff-tree --no-commit-id --name-only -r $sourceVersion)
     } else {
-        $files=$(git diff --no-commit-id --name-only -r $branchName $target)
+        $files=$(git diff --no-commit-id --name-only -r "$branchName" "$target")
     }
     $count=($files -split ' ').Length
     Write-Host "Total changed $count files"
