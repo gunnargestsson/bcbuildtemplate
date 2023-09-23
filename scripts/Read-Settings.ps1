cbec6628bbabd68f779e11d5a68c7943ba64b24f
Gunnar Thor Gestsson <gunnar@navision.guru>
2023-09-23 14:54:36 +0000

    try more fetch

--- scripts/Read-Settings.ps1
+++ scripts/Read-Settings.ps1
@@ -58,7 +58,8 @@ if ($changesOnly) {
     } else {
         Write-Host "Looking for changed files using 'git diff HEAD..$target --name-only'"
         git fetch origin $target
-        git diff HEAD..$target --name-only
+        git fetch origin $branchName
+        git diff $branchName..$target --name-only
         $files=$(git diff HEAD..$target --name-only)
     }
     $count=($files -split ' ').Length
